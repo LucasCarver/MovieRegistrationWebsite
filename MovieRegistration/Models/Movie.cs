13537aa3ad09e9449ba0af0ab419da074cea3b3c
LucasCarver <carver.lucas@gmail.com>
2020-05-22 00:35:08 -0400

    initial commit for movie registration website

---
+++ MovieRegistration/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string MovieTitle { get; set; }
+        public string Genre { get; set; }
+        public DateTime Year { get; set; }
+        public string Actors { get; set; }
+        public string Directors { get; set; }
+    }
+}
